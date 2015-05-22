ec354c027e50e11911b22321b416c4d4a34e8a9f
maryna <luv_this_world@hotmail.com>
2015-05-21 21:35:41 -0500

    hard mode

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,37 @@
 var text = document.getElementById('textBox');
 var clickButton = document.getElementById('myButton');
-var textArray =[];
 var sectionContent = document.getElementById('just-section');
+var textArray = [];
+var textArray = JSON.parse(localStorage.getItem('list'));
+render();
 
 function displayText (e) {
 	textArray.push(text.value);
 	render();
+	storage(textArray);
 } 
 
 clickButton.addEventListener('click', displayText);
 
+function pressEnter (e) {
+	if (e.keyCode == 13) {
+		textArray.push(text.value);
+	}
+	render();
+	storage(textArray);
+} 
+
+text.addEventListener('keyup', pressEnter);
+
 function render () {
-	sectionContent.innerHTML = '';
-	
 	var todoList = '';
-	var count = 1;
 	for (var i = 0; i<textArray.length; i++) {
-		todoList +=  count + '.' + textArray[i] + ' ';
-		count++;
+		todoList += textArray[i] + '<br>';
 	}
 	sectionContent.innerHTML = todoList;
 }
+
+function storage (textArray) {
+	localStorage.setItem('list', JSON.stringify(textArray));
+}
+
